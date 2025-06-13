353e7a824e94b177a71df8947753977765274787
shanurwan <wannurshafiqah18@gmail.com>
2025-06-14 01:26:37 +0800

    Create Logistic Analysis Query.sql

---
+++ Logistic Analysis Query.sql
@@ -0,0 +1,11 @@
+/* Count of delayed shipments by traffic status */
+
+SELECT 
+  Traffic_Status,
+  SUM(CASE WHEN Shipment_Status = 'Delayed' THEN 1 ELSE 0 END) AS Delayed_Count
+FROM 
+  shipment
+GROUP BY 
+  Traffic_Status;
+
+/* Number of orders placed per month */
